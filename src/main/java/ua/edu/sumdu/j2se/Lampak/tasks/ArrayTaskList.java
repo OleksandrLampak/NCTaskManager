b1dc57f9b88a11a329e45b32d39e6be56f3b3ecc
LAMPAK-NB <a.o.lampak@gmail.com>
2022-02-04 23:36:21 +0200

    ArrayTest - passed

---
+++ src/main/java/ua/edu/sumdu/j2se/Lampak/tasks/ArrayTaskList.java
@@ -0,0 +1,48 @@
+package ua.edu.sumdu.j2se.Lampak.tasks;
+
+import java.util.ArrayList;
+
+public class ArrayTaskList {
+
+    public ArrayList arrayTaskList = new ArrayList(10);
+
+    public void add(Task task) {
+        arrayTaskList.add(task);
+    }
+
+    public boolean remove(Task task) {
+        if (arrayTaskList.contains(task)) {
+            arrayTaskList.remove(task);
+            return true;
+        }
+        return false;
+    }
+
+    public int size() {
+        return arrayTaskList.size();
+    }
+
+    public Task getTask(int index) {
+        return (Task) arrayTaskList.get(index);
+    }
+
+    public ArrayTaskList incoming(int from, int to) {
+
+        ArrayTaskList listToReturn = new ArrayTaskList();
+        for (int i = 0; i < arrayTaskList.size(); i++) {
+            Task task = (Task) arrayTaskList.get(i);
+            if (task.active) {
+                if (!task.repeated) {
+                    if (task.time > from) {
+                        listToReturn.add(task);
+                    }
+                } else {
+                    if (task.startTime > from && task.endTime < to) {
+                        listToReturn.add(task);
+                    }
+                }
+            }
+        }
+        return listToReturn;
+    }
+}
